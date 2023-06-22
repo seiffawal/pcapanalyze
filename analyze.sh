00bb3a3d115a3539baed74d513f5714a623c08fd
seiffawal <83987697+seiffawal@users.noreply.github.com>
2023-06-22 15:33:30 +0300

    Update analyze.sh

--- analyze.sh
+++ analyze.sh
@@ -9,6 +9,10 @@ if [[ $1 == *.pcap ]]; then
     portscan_output=$(tshark -r "$1" -Y "tcp.flags.syn==1 && tcp.flags.ack==0" -T fields -e frame.number -e ip.src -e tcp.srcport)
     ping_output=$(tshark -r "$1" -Y "icmp" -T fields -e frame.number -e ip.src)
     download_output=$(tshark -r "$1" -Y "http.response.code==200" -T fields -e frame.number -e http.request.uri)
+    ddos_output=$(tshark -r "$1" -Y "ip.dst==your_server_ip && tcp.flags.syn==1" -T fields -e frame.number -e ip.src)
+    dos_output=$(tshark -r "$1" -Y "ip.dst==your_server_ip && (tcp.flags.syn==1 || icmp)" -T fields -e frame.number -e ip.src)
+    bufferoverflow_output=$(tshark -r "$1" -Y "tcp.flags.push==1 && data.data contains '41414141'" -T fields -e frame.number -e ip.src)
+    mitm_output=$(tshark -r "$1" -Y "ip.dst==your_server_ip && (tcp.flags.ack==1 && tcp.flags.syn==1) || (tcp.flags.rst==1 && tcp.flags.ack==1)" -T fields -e frame.number -e ip.src)
 
     # Display protocol types with their numbers
     echo "Protocol Types:"
@@ -50,6 +54,38 @@ if [[ $1 == *.pcap ]]; then
         done <<< "$download_output"
     else
         echo "No file downloads detected."
+    fi
+
+    # Check for DDoS attacks
+    if [[ -n "$ddos_output" ]]; then
+        echo "DDoS attacks detected:"
+        echo "$ddos_output"
+    else
+        echo "No DDoS attacks detected."
+    fi
+
+    # Check for DoS attacks
+    if [[ -n "$dos_output" ]]; then
+        echo "DoS attacks detected:"
+        echo "$dos_output"
+    else
+        echo "No DoS attacks detected."
+    fi
+
+    # Check for buffer overflow attacks
+    if [[ -n "$bufferoverflow_output" ]]; then
+        echo "Buffer overflow attacks detected:"
+        echo "$bufferoverflow_output"
+    else
+        echo "No buffer overflow attacks detected."
+    fi
+
+    # Check for Man-in-the-Middle attacks
+    if [[ -n "$mitm_output" ]]; then
+        echo "Man-in-the-Middle attacks detected:"
+        echo "$mitm_output"
+    else
+        echo "No Man-in-the-Middle attacks detected."
     fi
 
     echo "Analysis completed."
